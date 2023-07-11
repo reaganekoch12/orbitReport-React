2c45eafb3e99f1475011938120fb1e9b15768838
Reagan Koch <reaganekoch@gmail.com>
2023-07-11 16:02:38 -0500

    Updating Banner and App files

---
+++ public/Banner.js
@@ -0,0 +1,8 @@
+function banner() {
+    return `
+      <h1>Orbit Report</h1>
+      <p>Click on the buttons to see the satellites</p>
+    `;
+  }
+  
+  export { banner };
--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 
-
+import{banner} from '/.banner.js'
+//importing banner from banner file
 function App() {
   return (
     <div>
       <Buttons />
       <Table />
+    {banner()}
+//invoking banner function
     </div>
   );
 }
