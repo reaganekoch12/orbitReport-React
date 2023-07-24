9882c02042b56544a23bd5faa7d29ba8af2de034
Reagan Koch <reaganekoch@gmail.com>
2023-07-24 11:04:41 -0500

    updating table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,45 +1,28 @@
 import React from 'react';
-
-function Table ({ sat }) {
+function Table({ sat }) {
   return (
     <table>
       <thead>
-        
         <tr>
-        
           <th>Name</th>
           <th>Type of Satellite</th>
           <th>Launch Date</th>
           <th>Status</th>
-          
         </tr>
       </thead>
       <tbody>
-        {sat.map((data) => (
-          <tr key={data.id}>
-            //setting key equal to ID 
+        {sat.map((data, id) => (
+          <tr key={id}>
             <td>{data.name}</td>
-            //using data to get the satelite info. 
-            <td>{data.type}</td>
+            <td>{data.satelliteType}</td>
             <td>{data.launchDate}</td>
-            <td>{data.status ? 'Active' : 'Inactive'}</td>
-            //corresponds to Status tag (active or nonactive)
+            <td>{data.operational ? 'Active' : 'Inactive'}</td>
           </tr>
         ))}
       </tbody>
     </table>
   );
-};
-// Define PropTypes to specify the expected type of the sat prop
-Table.propTypes = {
-  sat: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      launchDate: PropTypes.string.isRequired,
-      status: PropTypes.bool.isRequired,
-    })
-  ).isRequired,
-};
+}
+
 export default Table;
+
